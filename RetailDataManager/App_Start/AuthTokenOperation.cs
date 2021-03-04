1edd05fc70daf384cb12a190a42182e18c758619
dion <dion.kllokoqi@gmail.com>
2021-03-04 23:00:02 +0100

    Add Swagger Documentation to API
    Adds Swashbuckle/Swagger to the API for documentation and testing.

---
+++ RetailDataManager/App_Start/AuthTokenOperation.cs
@@ -0,0 +1,55 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+// https://stackoverflow.com/questions/51117655/how-to-use-swagger-in-asp-net-webapi-2-0-with-token-based-authentication
+
+namespace RetailDataManager.App_Start
+{
+    public class AuthTokenOperation : IDocumentFilter
+    {
+        public void Apply(SwaggerDocument swaggerDoc, SchemaRegistry schemaRegistry, IApiExplorer apiExplorer)
+        {
+            // Document filter adds one new route called /token, puts it in the Auth category and the command is a post command. The type of data that is coming through is application/x-www-form-urlencoded, and the definiton of the three parameters is also specified. On the actual swagger page, these three parameters will be filled in.
+            swaggerDoc.paths.Add("/token", new PathItem
+            {
+                post = new Operation
+                {
+                    tags = new List<string> { "Auth" },
+                    consumes = new List<string>
+                    {
+                        "application/x-www-form-urlencoded"
+                    },
+                    parameters = new List<Parameter>
+                    {
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "grant_type",
+                            required = true,
+                            @in = "formData",
+                            @default = "password"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "username",
+                            required = false,
+                            @in = "formData"
+                        },
+                        new Parameter
+                        {
+                            type = "string",
+                            name = "password",
+                            required = false,
+                            @in = "formData"
+                        }
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ RetailDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,36 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace RetailDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        /// <summary>
+        /// This method will add a parameter to every operation.
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="schemaRegistry"></param>
+        /// <param name="apiDescription"></param>
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            // Get request has no parameter for example, since it is getting everything
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type = "string"
+            });
+        }
+    }
+}
