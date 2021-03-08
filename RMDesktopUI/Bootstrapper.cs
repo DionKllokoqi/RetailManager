455a615c9fed40971adbce3ed5884074afed86be
dion <dion.kllokoqi@gmail.com>
2021-03-08 23:23:18 +0100

    Add SImple Container
    Adds simple container DI system and configured it in Bootstrapper.

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -11,15 +11,50 @@ namespace RMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            // Return an instace if this internal container when we ask the container for a simple container
+            _container.Instance(_container);
+
+            // Singleton -> We create one instance of the class for the life of the application (or lifetime scope of the container. If, e.g., ShellViewModel asks for an IEventAggregator, it's gonna get the first EventAggregator ever created -> One instance for the application. Don't use Singletons to much!
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            // Don't use reflection too much! Bad for performance
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(
+                    viewModelType, viewModelType.ToString(), viewModelType));
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             // Launch ShellViewModel as our base view
             DisplayRootViewFor<ShellViewModel>();
+        }
+
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
